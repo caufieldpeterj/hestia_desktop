caae85746db3164760a69a4c4a27fc91f1ea21ed
peter caufield <caufield.peterj@gmail.com>
2021-05-13 20:52:50 -0400

    adding error page component

--- hestia_desktop/src/components/Nav.jsx
+++ hestia_desktop/src/components/Nav.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
-function Nav() {
+function Navbar() {
     return (
-        <div>
-            <nav>
-                <a href="">Home</a>
-                <a href="">All Listings</a>
-                <a href="">News</a>
-                <a href="">Mortgage Calculator</a>
-            </nav>
-            <h2>Test</h2>
-        </div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/'>Login</Link>
+                </li>
+            </ul>
+            <a href="">Home</a>
+            <a href="">All Listings</a>
+            <a href="">News</a>
+            <a href="">Mortgage Calculator</a>
+        </nav>
     )
 }
 
