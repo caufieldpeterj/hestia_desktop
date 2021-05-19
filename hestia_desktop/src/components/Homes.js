5e997214a6123bf60edbd50d7cba2d7f82f33d08
peter caufield <caufield.peterj@gmail.com>
2021-05-19 18:35:23 -0400

    useState hook in place within Homes component, moving up to App.js to avoid having to lift up state

--- hestia_desktop/src/components/Homes.js
+++ hestia_desktop/src/components/Homes.js
@@ -1,25 +1,27 @@
-const homes = [
-    {
-        id: 1,
-        town: 'Cherry Hill',
-        price: 575000,
-        reminder: true
-    },
-    {
-        id: 2,
-        town: 'Ocean City',
-        price: 900000,
-        reminder: true
-    },
-    {
-        id: 3,
-        town: 'Sarasota',
-        price: 650000,
-        reminder: false
-    },
-];
+import { useState } from 'react';
 
 const Homes = () => {
+    const [homes, setHomes] = useState([
+            {
+                id: 1,
+                town: 'Cherry Hill',
+                price: 575000,
+                reminder: true
+            },
+            {
+                id: 2,
+                town: 'Ocean City',
+                price: 900000,
+                reminder: true
+            },
+            {
+                id: 3,
+                town: 'Sarasota',
+                price: 650000,
+                reminder: false
+            },
+        ]
+    )
     return (
         <>
             {homes.map((home) => (
